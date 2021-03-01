868629577590d05e1b60959639a5ca161f4c0776
Git Server Admin <admin@git-server>
2021-03-01 02:05:51 +0000

    Add initial repository

---
+++ src/main/java/com/hackerrank/eshopping/product/dashboard/controller/ProductsController.java
@@ -0,0 +1,10 @@
+package com.hackerrank.eshopping.product.dashboard.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
+public class ProductsController {
+
+}
